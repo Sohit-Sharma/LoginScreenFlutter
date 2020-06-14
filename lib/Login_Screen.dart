cf3225fc97cf07591671bb28674bf6c32d48a0f6
Sohit Sharma <Sohitsharma922@gmail.com>
2020-06-14 23:12:56 +0530

    Initial commit

---
+++ lib/Login_Screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+ class LoginPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new Login();
+ }
+
+class Login extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'LOG IN',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'UserName',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
+
+              },
+              textColor: Colors.blue,
+              child: Text('Forgot Password'),
+            ),
+            Container(
+                height: 50,
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.blue,
+                  child: Text('Login'),
+                  onPressed: () {
+
+                  },
+                )),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Text('Does not have account?'),
+                  FlatButton(
+                    textColor: Colors.blue,
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: (){
+
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
